b41adfc42466b59f28d2e131d5cd329a1ab2f51e
Akshhay <=>
2024-06-28 23:03:05 +0530

    fixed apos error

--- components/HowItWorks.jsx
+++ components/HowItWorks.jsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable react/no-unescaped-entities */
 import React, { useRef } from "react";
 import { chipImg, frameImg, frameVideo } from "../utils";
 import { useGSAP } from "@gsap/react";
@@ -47,7 +48,7 @@ const HowItWorks = () => {
           </h2>
 
           <p className="hiw-subtitle">
-            It's here. The biggest redesign in the history of Apple GPUs.
+            It&apos;s here. The biggest redesign in the history of Apple GPUs.
           </p>
         </div>
 
